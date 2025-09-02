3b8fea333600df1f07f9e2b3c5b25ac072e7700b
MacBook Air <Jcstudios.database@gmail.com>
2025-09-03 02:46:43 +0800

    try

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
     siteName: "WebWorks",
     images: [
       {
-        url: "https://ronnelsantos.vercel.app/banner.jpg",
+        url: "https://ronnelsantos.vercel.app/ban.jpg",
       },
     ],
     type: "website",
