b5e7e57a50eb012e54cb5c8892c2b8f959c41f31
MacBook Air <Jcstudios.database@gmail.com>
2025-09-04 01:43:17 +0800

    fix icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,14 @@ export const metadata: Metadata = {
   description:
     "Empowering Your Business Through Intelligent Web Solutions. We push boundaries to deliver exceptional results for clients and partners.",
   authors: [{ name: "Ronnel Santos" }],
+  icons: {
+    icon: [
+      { url: "/favicon.ico", type: "image/x-icon" },
+      { url: "/favicon-96x96.png", sizes: "96x96", type: "image/png" },
+      { url: "/favicon.svg", type: "image/svg+xml" },
+    ],
+    apple: [{ url: "/apple-touch-icon.png", sizes: "180x180" }],
+  },
   openGraph: {
     title: "WebWorks | Intelligent Web Solutions",
     description:
@@ -56,28 +64,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" data-scroll-behavior="smooth" className={`${alumni.variable} ${russo.variable}`}>
       <head>
-        {/* Favicon set */}
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-
-        {/* Apple icons */}
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <meta name="apple-mobile-web-app-title" content="Webworks" />
-
-      
-
         {/* Google Analytics */}
         <Script
           strategy="afterInteractive"
